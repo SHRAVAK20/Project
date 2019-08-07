1501c0d3b12e685ed943ccfb4cac7102689ad849
SHRAVAK20 <46772799+SHRAVAK20@users.noreply.github.com>
2019-08-07 16:40:10 +0530

    commit 5

--- MT2018001_MT2018111_TCPClient.java
+++ MT2018001_MT2018111_TCPClient.java
@@ -30,3 +30,33 @@ System.out.println(dis.readFloat());
 
 	}
 }
+
+
+class MT2018001_MT2018111_2_TCPClient2{
+	public static void main(String args[]) throws IOException{
+Socket c=new Socket("192.168.43.65",1254);
+
+DataInputStream dis=new DataInputStream(c.getInputStream());
+DataOutputStream dos=new DataOutputStream(c.getOutputStream());
+
+Random rand=new Random();
+ for(int i=0;i<5;i++)
+ {
+ 	
+ 	dos.writeInt(rand.nextInt(100)+1);
+ }
+ int a;
+ System.out.println("marks are:");
+ for(int i=0;i<5;i++)
+ {
+ 	a=dis.readInt();
+ 	System.out.println(a);
+}
+System.out.println("average is:");
+System.out.println(dis.readFloat());
+ dis.close();
+ dos.close();
+ c.close();
+
+	}
+}
